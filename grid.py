5f9eb3584dee50c36eed71b30a0b802925a2e51f
Lukakao <138331238+Lukakao@users.noreply.github.com>
2023-10-05 00:39:13 -0300

    quetd

--- grid.py
+++ grid.py
@@ -5,22 +5,39 @@ class Grid:
     def __init__(self, w, h):
         self.w = w
         self.h = h
-        self.grid = [[Celda()]*w]*h
+        self.grid = [[Celda() for i in range(h)] for j in range(w)] 
 
 
     def mostrar(self):
         for x in range(0,self.h):
             s = ''
+            a = ''
+            b = ' '
             for y in range(0,self.w):
-                s = s + str(self.grid[x][y].num)
+                num = self.grid[x][y].getNum()
+                if num != -1: 
+                    a = ' '
+                    b = ''
+                s = s + a + str(num) + b
             print(s)
+
+    def set_celda(self,x,y, n):
+        self.grid[x][y].setNum(n)
 
 class Celda:
     def __init__(self):
         self.conocida = False
         self.num = -1
         self.lista = False
+    
+    def setNum(self, n):
+        self.num = n
+    
+    def getNum(self):
+        return self.num
+    
 
 
-g = Grid(5,16)
-g.mostrar()
+
+#g = Grid(5,16)
+#g.mostrar()
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pyautogui
 import time
 import random 
+from grid import Grid
+from grid import Celda
 pyautogui.FAILSAFE = True
 
 def isDead():
@@ -18,9 +20,17 @@ def rPos():
 
 
 pL = pyautogui.center(pyautogui.locateOnScreen("izq.PNG"))
+pL = (pL[0]+6, pL[1]-2, 0, 0)
+pyautogui.moveTo(pL)
+
 pR = pyautogui.center(pyautogui.locateOnScreen("der.PNG"))
+pR = (pR[0]-4, pR[1]-3, 0, 0)
+pyautogui.moveTo(pR)
+
 pA = pyautogui.center(pyautogui.locateOnScreen("arriba.PNG"))
 pA = (pA[0]+6+9, pA[1]+6+4, 0, 0)
+pyautogui.moveTo(pA)
+
 pS = pyautogui.center(pyautogui.locateOnScreen("smiley.PNG"))
 pyautogui.click(pS[0]-4,pS[1]+6)
 pyautogui.click()
@@ -33,14 +43,14 @@ def color(x,y):
     return c
 
 # tile size 16px
-size_h = int((pL[1]-pA[1])/16)
+size_h = int((pL[1]-pA[1]+4)/16)
 size_w = int((pR[0]-pL[0])/16)
 
 print(size_h)
 print(size_w)
 
 
-# (192,192,192) -> 0 y si 6 pixeles arriba es (255,255,255) -> sin clickear
+# (192,192,192) -> 0 y si 6 pixeles arriba es (255,255,255) -> sin clickear (9)
 # (0,0,255)     -> 1
 # (0,128,0)     -> 2
 # (255,0,0)     -> 3
@@ -50,13 +60,40 @@ print(size_w)
 # (0, 0, 0)     -> 7 revisar si se murio primero
 # (0, 0, 0) -> mina
 
+
+grid = Grid(size_w,size_h)
+
+def actualizar_grid(gr, color, x, y):
+    if color == (192,192,192):
+        if pyautogui.pixel(x*16,y*16+6) == (255,255,255):
+            gr.set_celda(x,y,9)
+        else:
+            gr.set_celda(x,y,0)
+    elif color == (0,0,255):
+        gr.set_celda(x,y,1)
+
+    elif color == (0,128,0):
+        gr.set_celda(x,y,2)
+
+    elif color == (255,0,0):
+        gr.set_celda(x,y,3)
+
+    elif color == (0, 0, 128):
+        gr.set_celda(x,y,4)
+
+
 for a in range(0,10):
     x,y = rPos()
+    print("rpos ", x,y)
     click(x,y)
     if isDead(): break
     c = color(x,y)
     print(c)
+    actualizar_grid(grid,c, x, y)
 
+
+grid.mostrar()
+print("aa")
 #for x in range(0,16,2):
  #   for y in range(0,16,2):
    #     pyautogui.moveTo(pA[0]+x,pA[1]+y)
